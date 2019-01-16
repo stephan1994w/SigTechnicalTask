bb9cebb9d5e6c20c976ed67592bb8f363f2d814d
Stephan Williams <stephan1994w@gmail.com>
2019-01-16 22:17:10 +0000

    Initial Commit

---
+++ Assets/GridCreator.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridCreator : MonoBehaviour {
+
+
+    public GameObject GridSquare;
+    public GameObject GridCorners;
+    public int GridXSize;
+    public int GridYSize;
+
+    float xDisplacement, yDisplacement;
+
+    GameObject[,] grid;
+
+    GameObject[,] corners;
+
+    // Use this for initialization
+    void Start () {
+        xDisplacement = (float)GridXSize / 2;
+
+        yDisplacement = (float)GridYSize / 2;
+
+        generateTiles();
+
+    }
+	
+	void LateUpdate () {
+
+    }
+
+    void generateTiles()
+    {
+        grid = new GameObject[GridXSize, GridYSize];
+
+        for (int i = 0; i < GridXSize; i++)
+        {
+            for(int j = 0; j < GridYSize; j++)
+            {
+                grid[i, j] = Instantiate(GridSquare, new Vector3((i - xDisplacement) * 2 , (j - yDisplacement) * 2, 0), Quaternion.identity);
+            }
+        }
+
+        corners = new GameObject[GridXSize + 1, GridYSize + 1];
+        for (int i = 0; i <= GridXSize; i++)
+        {
+            for (int j = 0; j <= GridYSize; j++)
+            {
+                corners[i, j] = Instantiate(GridCorners, new Vector3((i - xDisplacement) * 2, (j - yDisplacement) * 2, 0), Quaternion.identity);
+            }
+        }
+    }
+}
